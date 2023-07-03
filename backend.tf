8c27ca88399fdef06411b7f08de8a01128e532b0
andyxuan2010 <andyxuan2010@gmail.com>
2023-07-03 15:27:35 -0400

    init setup

---
+++ backend.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = ">=1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.58.0"
+    }
+    azapi = {
+      source = "Azure/azapi"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "PayAsYouGo"
+    storage_account_name = "azgeneraluse"
+    container_name       = "tfstate"
+    key                  = "k8s-demo.tfstate"
+  }
+
+}
+
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_client_config" "current" {}
+#data.azurerm_client_config.current.client_id
+data "azurerm_subscriptions" "available" {}
+#data.azurerm_subscriptions.available.subscriptions
---
+++ main.tf
@@ -0,0 +1,23 @@
+
+resource "azurerm_resource_group" "challenge-rg" {
+  name     = "challenge-resources"
+  location = "West Europe"
+  tags = merge(var.common_tags, {
+    name = "challenge-rg"
+  })
+}
+resource "azurerm_virtual_network" "challenge-vnet" {
+  name                = "challenge-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.challenge-rg.location
+  resource_group_name = azurerm_resource_group.challenge-rg.name
+  tags = merge(var.common_tags, {
+    name = "challenge-vnet"
+  })
+}
+resource "azurerm_subnet" "challenge-subnet" {
+  name                 = "challenge-subnet"
+  resource_group_name  = azurerm_resource_group.challenge-rg.name
+  virtual_network_name = azurerm_virtual_network.challenge-vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
